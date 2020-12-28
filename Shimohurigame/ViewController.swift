cd401a68f5d4c63536592b5d723d271856adb6ef
miyuki24 <miyuki.0424@yahoo.ne.jp>
2020-12-28 13:18:06 +0900

    Initial Commit

---
+++ Shimohurigame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shimohurigame
+//
+//  Created by 田中美幸 on 2020/12/28.
+//  Copyright © 2020 miyuki.tanaka2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
